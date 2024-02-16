fff27da0abc296f6025adf0f962e6e5a4455770a
Gautam <bhaiyakaleen18@gmail.com>
2024-02-16 10:32:07 +0530

    16-02-2024

--- services/favorite.service.js
+++ services/favorite.service.js
@@ -30,7 +30,12 @@ module.exports = class FavoriteServices {
 
     async getAllFavorites(body) {
         try {
-            return await Favorites.find(body);
+            let results = await Favorites.find(body).populate('favoriteItem').populate({
+                path: 'user',
+                model: 'users',
+                select: 'firstName lastName email'
+            });
+            return results;
         } catch (error) {
             console.log(error);
             return res.json({ message: "Server Error from Favorites Services" });
