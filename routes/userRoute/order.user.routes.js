aa1737ffb9b64bfeed83931607a0e98ee8bf2615
Gautam <bhaiyakaleen18@gmail.com>
2024-02-21 18:36:37 +0530

    21-02

--- routes/userRoute/order.user.routes.js
+++ routes/userRoute/order.user.routes.js
@@ -4,8 +4,8 @@ const { createOrder, getOrder, getAllOrder, deleteOrder, getOrderById } = requir
 const { userVerifyToken } = require('../../helper/userToken/userVerifytoken');
 
 Order.post('/add-Order',userVerifyToken,createOrder);
-Order.get('/get-Order',userVerifyToken,getOrder);
-Order.get('/getOrder',userVerifyToken,getOrderById);
+// Order.get('/get-Order',userVerifyToken,getOrder);
+Order.get('/get-Order',userVerifyToken,getOrderById);
 Order.get('/get-All-Order',getAllOrder);
 Order.delete('/delete-Order',userVerifyToken,deleteOrder);
 
